6e4fa18c41b5d5c6e54498692b3348eaff8b087a
hwangchahae <wndal12@gmail.com>
2025-06-09 16:44:06 +0900

    modify

--- app.py
+++ app.py
@@ -22,13 +22,19 @@ with col1:
                 # 멀티모달 모델을 이용해서 사진의 요리의 ;특성을 분석
                 # 출력
                 st.markdown('### 요리의 맛과 향 분석 결과')
-                
+                # 해당 함수 호출
+                dish_flavor = 
+                st.info(dish_flavor)
             with st.spinner('2단계 : 요리에 어울리는 와인 리뷰를 검색하는 중...'):
                 # 요리의 특성정보로 와인을 추천ai 동작
                 st.markdown('### 와인 리뷰 검색 결과')
-                
+                # 해당 함수 호출
+                wine_search_result = 
+                st.txt(wine_search_result['wine_reviews'])
             with st.spinner('3단계 : AI소믈리에가 와인 페어링에 대한 추천글을 생성하는 중...'):
                 # LLM을 이용해서 추천글을 생성
                 st.markdown('### AI 소믈리에 와인페어링 추천')
-                
+                # 해당 함수 호출
+                wine_recommandation = 
+                st.info(wine_recommandation)
         st.success('추천이 완료되었습니다.')    
